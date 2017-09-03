0ac1d78631f146ef778187ee0222d2ff02beacaf
lex <lexaprog447@gmail.com>
2017-09-03 22:49:34 +0300

    +argument parser

--- squash_merge.py
+++ squash_merge.py
@@ -2,8 +2,18 @@
 
 import sys
 from subprocess import check_call, check_output
+from argparse import ArgumentParser
 
-SQUASH_COMMIT_MSG = "merge(squash) branch"
+#------------------------------------------------------------------------------
+
+def parse_commandline():
+    parser = ArgumentParser(description='merge with squash branch1 to branch2')
+    parser.add_argument(dest='branch1',
+                        help='branch that will be merged')
+    parser.add_argument(dest='branch2',
+                        help='merge destination branch')
+    args = parser.parse_args()
+    return args
 
 #------------------------------------------------------------------------------
 
@@ -13,19 +23,19 @@ def get_last_commit_msg():
 #------------------------------------------------------------------------------
 
 def main():
-    branch1, branch2 = sys.argv[1:3]
-
-    check_call('git checkout %s' % branch2, shell=True)
+    args = parse_commandline()
+    check_call('git checkout %s' % args.branch2, shell=True)
 
     # check if already merged
+    SQUASH_COMMIT_MSG = "merge(squash) branch"
     last_commit_msg = get_last_commit_msg()
     if last_commit_msg.startswith(SQUASH_COMMIT_MSG):
         last_merged_branch = last_commit_msg.split()[-1]
-        if last_merged_branch == branch1:
+        if last_merged_branch == args.branch1:
             check_call('git reset --hard HEAD~', shell=True)
 
-    check_call('git merge --squash %s' % branch1, shell=True)
-    check_call('git commit -m "%s %s"' % (SQUASH_COMMIT_MSG, branch1), shell=True)
+    check_call('git merge --squash %s' % args.branch1, shell=True)
+    check_call('git commit -m "%s %s"' % (SQUASH_COMMIT_MSG, args.branch1), shell=True)
 
 #------------------------------------------------------------------------------
 
